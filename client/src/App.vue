34541053e1b07e69ef26219bc7533c2a2ccf9d91
Jonny Reading <jonnyreading@gmail.com>
2020-09-04 19:54:46 +0100

    import components

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,16 @@
 </template>
 
 <script>
+import LoginPage from "@/components/Login.vue";
+import QuestionsPage from "@/components/Questions.vue";
+import ResultsPage from "@/components/Results.vue";
 export default {
+  name: "app",
+  components: {
+    ResultsPage,
+    QuestionsPage,
+    LoginPage
+  }
 
 }
 </script>
