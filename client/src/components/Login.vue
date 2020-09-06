527ca0790b91b243e58ae9b4a2265b8e4a1419dc
Jonny Reading <jonnyreading@gmail.com>
2020-09-06 12:14:50 +0100

    use countries list for a dropdown instead of country text entry

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,23 +1,30 @@
 <template>
-<div><select id="select-user" v-model="selected">
-  <option value=null disabled >Please choose an option</option>
-  <option value="new_user">NEW USER</option>
-  <option value=null disabled >----</option>
+<div id="login-form">
+  <select id="select-user" v-model="selected">
+    <option value=null disabled >Please choose an option</option>
+    <option value="new_user">NEW USER</option>
+    <option value=null disabled >----</option>
 
-  <option v-for="(user, index) in users" :key="index" :value="user">{{ user.name}}</option>
-
+    <option v-for="(user, index) in users" :key="index" :value="user">{{ user.name}}</option>
   </select>
-  <div  v-if="selected==='new_user'">
-    <label for="name">Name:</label>
-    <input type="text" v-model="name" id="name">
-    <label for="country">Country:</label>
-    <input type="text" v-model="country" id="country">
-  </div>
-  <div v-else-if="selected">
-    <p>{{ selected.name }}</p>
-    <p>{{ selected.country}}</p>
-  </div>
-  <input v-if="selected" type="button" :value="selected==='new_user'? 'Create New User':'Load my details'" v-on:click="handleClick">
+
+  <form v-on:submit.prevent="handleForm">
+    <div  v-if="selected==='new_user'">
+      <label for="name">Name:</label>
+      <input type="text" v-model="name" id="name" required>
+      <label for="country">Country:</label>
+      <!-- <input type="text" v-model="country" id="country"> -->
+      <select v-model="country" required>
+        <option value="" disabled>Choose a country...</option>
+        <option v-for="(nation, index) in countries" :key="index" :value="nation.name">{{nation.name}}</option>
+      </select>
+    </div>
+    <div v-else-if="selected">
+      <p>{{ selected.name }}</p>
+      <p>{{ selected.country}}</p>
+    </div>
+    <input v-if="selected" type="submit" :value="selected === 'new_user' ? 'Create New User' : 'Load my details'">
+  </form>
 </div>
 
 </template>
@@ -28,7 +35,7 @@ import UserService from "@/services/UserService.js";
 
 export default {
   name: "login-page",
-  props: ["users"],
+  props: ["users", "countries"],
   data(){
     return {
       name: "",
@@ -38,7 +45,7 @@ export default {
   },
 
 methods: {
-  handleClick: function() {
+  handleForm: function() {
     if (this.selected === "new_user") {
       const newUser = {
         name: this.name,
