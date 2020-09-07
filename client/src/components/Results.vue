e8ff35f5d7de1ad35ebf12322d0f2493cd5784e7
Jonny Reading <jonnyreading@gmail.com>
2020-09-07 10:01:00 +0100

    moved results dial into a child component

--- client/src/components/Results.vue
+++ client/src/components/Results.vue
@@ -1,50 +1,15 @@
 <template>
-<div>
-  <h3>Your Carbon Footprint (tonnes of CO2 per annum)</h3>
-  <GChart id="gauge-chart" :type="chartType" :settings="chartSettings" :data="currentUser" :options="chartOptions"/>
-</div>
+  <ResultsDial :selectedUser="selectedUser" />
 </template>
 
 <script>
-import { GChart } from 'vue-google-charts';
-
+import ResultsDial from "@/components/ResultsDial.vue";
 export default {
-    name: "results-page",
-    components: {
-    GChart
-  },
-  data () {
-    return {
-      chartOptions: {
-        width: 200,
-        height:200,
-        redFrom:12, redTo: 15,
-        yellowFrom:7.5, yellowTo: 12,
-        minorTicks: 1,
-        max:15
-      },
-      chartType:"Gauge",
-      chartSettings:{
-        packages:['corechart', 'gauge']
-      }
-    }
-  },
-  props: ['selectedUser'],
-  computed: {currentUser: function(){
-      if (this.selectedUser) {
-        return this.getUserScore(this.selectedUser);
-      }
-      return null
-  }},
-  methods: {
-    getUserScore: function(cUser){
-      const currentUser=[['label','score']];
-      const newUserScore=['C02',cUser.score];
-      currentUser.push(newUserScore);
-      return currentUser;
-    }
+  name: "results",
+  props: ["selectedUser"],
+  components: {
+    ResultsDial
   }
- 
 
 }
 </script>
