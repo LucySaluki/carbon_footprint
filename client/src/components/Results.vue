84b6f2b7340d88d047079c33c848c341d61f8f27
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-09-07 18:57:19 +0100

    sorted out inline dials

--- client/src/components/Results.vue
+++ client/src/components/Results.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <h3>Carbon Footprint comparative results (tonnes of CO2 per annum)</h3>
-    <ResultsDial :data="newUserScore" />
-    <p>Your Results</p>
-    <ResultsDial v-if="globalEmissions" :data="newCountryScore"/>
-    <p>The Average for Your Country</p>
+    <p class="footnote">(please be aware it may take a few seconds for your country data to load)</p>
+    <div class="dials">  
+      <p class="your">Your Results</p>
+      <ResultsDial :data="newUserScore" />
+      <p class="your-country" v-if="globalEmissions">The Average for Your Country</p>
+      <ResultsDial v-if="globalEmissions" :data="newCountryScore"/>
+    </div>
     <UpdateForm :selectedUser="selectedUser" :questions="questions" /> 
   </div>
 </template>
@@ -30,7 +33,7 @@ export default {
   methods: {
     getUserScore: function(cUser){
       const countryUser=cUser.country;
-      const newUserValue=['Your C02',cUser.score];
+      const newUserValue=['C02',cUser.score];
       this.newUserScore.push(newUserValue);
     }
   },
@@ -39,13 +42,34 @@ export default {
       const currUser=this.selectedUser.country;
       const emissionCountries=this.globalEmissions;
       const countryAvg = emissionCountries.find(el => el.country===currUser);
-      const newCountryValue=['Avg.CO2',countryAvg.avg];
+      const newCountryValue=['CO2',countryAvg.avg];
       this.newCountryScore.push(newCountryValue);
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.your {
+  grid-area: title1;
+  justify-self:center;
+}
+.your-country {
+  grid-area:title2;
+}
+.dials > ResultsDial {
+  grid-area: dial;
+}
+.dials {
+  display: grid;
+  grid-template-columns: 200px 300px;
+  grid-template-rows: auto;
+  grid-template-areas: 
+    "dial dial "
+    "title1 title2";
+}
+.footnote {
+  font-size: 75%;
+  font-style: italic;
+}
 </style>
