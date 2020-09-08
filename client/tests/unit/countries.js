f0d1286d28cb3500da44247f06949e16557c59a1
Duncan Ryan <duncancryan1@gmail.com>
2020-09-08 15:11:35 +0100

    created countries dummy data

---
+++ client/tests/unit/countries.js
@@ -0,0 +1,253 @@
+export const countriesData = [
+    {
+        name: 'Albania',
+        topLevelDomain: [
+            '.al'
+        ],
+        alpha2Code: 'AL',
+        alpha3Code: 'ALB',
+        callingCodes: [
+            '355'
+        ],
+        capital: 'Tirana',
+        altSpellings: [
+            'AL',
+            'Shqipëri',
+            'Shqipëria',
+            'Shqipnia'
+        ],
+        region: 'Europe',
+        subregion: 'Southern Europe',
+        population: 2886026,
+        latlng: [
+            41,
+            20
+        ],
+        demonym: 'Albanian',
+        area: 28748,
+        gini: 34.5,
+        timezones: [
+            'UTC+01:00'
+        ],
+        borders: [
+            'MNE',
+            'GRC',
+            'MKD',
+            'KOS'
+        ],
+        nativeName: 'Shqipëria',
+        numericCode: '008',
+        currencies: [
+            {
+                code: 'ALL',
+                name: 'Albanian lek',
+                symbol: 'L'
+            }
+        ],
+        languages: [
+            {
+                iso639_1: 'sq',
+                iso639_2: 'sqi',
+                name: 'Albanian',
+                nativeName: 'Shqip'
+            }
+        ],
+        translations: {
+            de: 'Albanien',
+            es: 'Albania',
+            fr: 'Albanie',
+            ja: 'アルバニア',
+            it: 'Albania',
+            br: 'Albânia',
+            pt: 'Albânia',
+            nl: 'Albanië',
+            hr: 'Albanija',
+            fa: 'آلبانی'
+        },
+        flag: 'https://restcountries.eu/data/alb.svg',
+        regionalBlocs: [
+            {
+                acronym: 'CEFTA',
+                name: 'Central European Free Trade Agreement',
+                otherAcronyms: [],
+                otherNames: []
+            }
+        ],
+        cioc: 'ALB'
+    },
+    {
+        name: 'Algeria',
+        topLevelDomain: [
+            '.dz'
+        ],
+        alpha2Code: 'DZ',
+        alpha3Code: 'DZA',
+        callingCodes: [
+            '213'
+        ],
+        capital: 'Algiers',
+        altSpellings: [
+            'DZ',
+            'Dzayer',
+            'Algérie'
+        ],
+        region: 'Africa',
+        subregion: 'Northern Africa',
+        population: 40400000,
+        latlng: [
+            28,
+            3
+        ],
+        demonym: 'Algerian',
+        area: 2381741,
+        gini: 35.3,
+        timezones: [
+            'UTC+01:00'
+        ],
+        borders: [
+            'TUN',
+            'LBY',
+            'NER',
+            'ESH',
+            'MRT',
+            'MLI',
+            'MAR'
+        ],
+        nativeName: 'الجزائر',
+        numericCode: '012',
+        currencies: [
+            {
+                code: 'DZD',
+                name: 'Algerian dinar',
+                symbol: 'د.ج'
+            }
+        ],
+        languages: [
+            {
+                iso639_1: 'ar',
+                iso639_2: 'ara',
+                name: 'Arabic',
+                nativeName: 'العربية'
+            }
+        ],
+        translations: {
+            de: 'Algerien',
+            es: 'Argelia',
+            fr: 'Algérie',
+            ja: 'アルジェリア',
+            it: 'Algeria',
+            br: 'Argélia',
+            pt: 'Argélia',
+            nl: 'Algerije',
+            hr: 'Alžir',
+            fa: 'الجزایر'
+        },
+        flag: 'https://restcountries.eu/data/dza.svg',
+        regionalBlocs: [
+            {
+                acronym: 'AU',
+                name: 'African Union',
+                otherAcronyms: [],
+                otherNames: [
+                    'الاتحاد الأفريقي',
+                    'Union africaine',
+                    'União Africana',
+                    'Unión Africana',
+                    'Umoja wa Afrika'
+                ]
+            },
+            {
+                acronym: 'AL',
+                name: 'Arab League',
+                otherAcronyms: [],
+                otherNames: [
+                    'جامعة الدول العربية',
+                    'Jāmiʻat ad-Duwal al-ʻArabīyah',
+                    'League of Arab States'
+                ]
+            }
+        ],
+        cioc: 'ALG'
+    },
+    {
+        name: 'Angola',
+        topLevelDomain: [
+            '.ao'
+        ],
+        alpha2Code: 'AO',
+        alpha3Code: 'AGO',
+        callingCodes: [
+            '244'
+        ],
+        capital: 'Luanda',
+        altSpellings: [
+            'AO',
+            'República de Angola',
+            'ʁɛpublika de an\'ɡɔla'
+        ],
+        region: 'Africa',
+        subregion: 'Middle Africa',
+        population: 25868000,
+        latlng: [
+            -12.5,
+            18.5
+        ],
+        demonym: 'Angolan',
+        area: 1246700,
+        gini: 58.6,
+        timezones: [
+            'UTC+01:00'
+        ],
+        borders: [
+            'COG',
+            'COD',
+            'ZMB',
+            'NAM'
+        ],
+        nativeName: 'Angola',
+        numericCode: '024',
+        currencies: [
+            {
+                code: 'AOA',
+                name: 'Angolan kwanza',
+                symbol: 'Kz'
+            }
+        ],
+        languages: [
+            {
+                iso639_1: 'pt',
+                iso639_2: 'por',
+                name: 'Portuguese',
+                nativeName: 'Português'
+            }
+        ],
+        translations: {
+            de: 'Angola',
+            es: 'Angola',
+            fr: 'Angola',
+            ja: 'アンゴラ',
+            it: 'Angola',
+            br: 'Angola',
+            pt: 'Angola',
+            nl: 'Angola',
+            hr: 'Angola',
+            fa: 'آنگولا'
+        },
+        flag: 'https://restcountries.eu/data/ago.svg',
+        regionalBlocs: [
+            {
+                acronym: 'AU',
+                name: 'African Union',
+                otherAcronyms: [],
+                otherNames: [
+                    'الاتحاد الأفريقي',
+                    'Union africaine',
+                    'União Africana',
+                    'Unión Africana',
+                    'Umoja wa Afrika'
+                ]
+            }
+        ],
+        cioc: 'ANG'
+    }
+]
