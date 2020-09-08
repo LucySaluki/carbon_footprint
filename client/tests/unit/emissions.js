e63b8add80a390bd50b80bb452b459b7ad22c2d2
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-09-08 17:19:45 +0100

    map tests completed

---
+++ client/tests/unit/emissions.js
@@ -0,0 +1,47 @@
+export const emissionsData = [
+    {
+      country: 'Albania',
+      countryCode: 'ALB',
+      countryShortCode: 'AL',
+      border: [
+        'MNE',
+        'GRC',
+        'MKD',
+        'KOS'
+      ],
+      emissions: 4555307.493,
+      population: 2886026,
+      avg: 1.578
+    },
+    {
+      country: 'Algeria',
+      countryCode: 'DZA',
+      countryShortCode: 'DZ',
+      border: [
+        'TUN',
+        'LBY',
+        'NER',
+        'ESH',
+        'MRT',
+        'MLI',
+        'MAR'
+      ],
+      emissions: 142315117.813,
+      population: 40400000,
+      avg: 3.523
+    },
+    {
+      country: 'Angola',
+      countryCode: 'AGO',
+      countryShortCode: 'AO',
+      border: [
+        'COG',
+        'COD',
+        'ZMB',
+        'NAM'
+      ],
+      emissions: 19379906.135,
+      population: 25868000,
+      avg: 0.749
+    }
+  ]
