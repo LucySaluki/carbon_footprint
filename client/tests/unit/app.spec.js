97c2397070e246775c8c2239c36332917f361cd0
Duncan Ryan <duncancryan1@gmail.com>
2020-09-08 15:19:13 +0100

    first test passing

--- client/tests/unit/app.spec.js
+++ client/tests/unit/app.spec.js
@@ -1,14 +1,14 @@
-import { shallowMount } from '@vue/test-utils';
-import App from '@/App.vue';
-import { usersData } from './users';
-import { emissionsData } from './emissions';
-import { questionsData } from './questions';
+// import { shallowMount } from '@vue/test-utils';
+// import App from '@/App.vue';
+// import { usersData } from './users';
+// import { emissionsData } from './emissions';
+// import { questionsData } from './questions';
 
-global.fetch = jest.fn(() => 
-  Promise.resolve({
-      json: () => Promise.resolve({}),
-  })
-);
+// global.fetch = jest.fn(() => 
+//   Promise.resolve({
+//       json: () => Promise.resolve({}),
+//   })
+// );
 
 // when we do beforeEach, set data in the wrapper
-// await wrapper.setData({})
+// await wrapper.setData({})gaa
--- client/tests/unit/login.spec.js
+++ client/tests/unit/login.spec.js
@@ -1,5 +1,21 @@
 import { shallowMount } from '@vue/test-utils';
 import Login from '@/components/Login.vue';
-import { userData } from './users';
-import { questionsData } from './questions';
+import { userData, usersData } from './users';
+import { countriesData } from './countries';
 
+describe('Login.vue', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(Login, {
+        propsData: {
+            users: usersData,
+            countries: countriesData
+        }
+    });
+  });
+
+it('should have a country prop.', () => {
+  expect(wrapper.vm.countries[0].name).toMatch('Albania')
+});
+
+})
