5545682c100414f8f58316d55e1d12bbb1dfe45d
Jonny Reading <jonnyreading@gmail.com>
2020-09-08 12:11:10 +0100

    fix map not displaying data

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-    <h3>World Carbon Dioxide Production per Capita (tonnes)</h3>
+        <h3>MapTitle</h3>
         <GChart
             id="gauge-chart"
             type="GeoChart"
