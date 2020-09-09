57aec3a54597e28bd2d06765413adef1b82500b7
Duncan Ryan <duncancryan1@gmail.com>
2020-09-09 01:09:50 +0100

    imported data, component and shallowMount

---
+++ client/tests/unit/results.spec.js
@@ -0,0 +1,5 @@
+import { shallowMount } from '@vue/test-utils';
+import Results from '@/components/Results.vue';
+import { questionsData } from './questions.';
+import { usersData } from './users';
+import { emissionsData } from './emissions'
