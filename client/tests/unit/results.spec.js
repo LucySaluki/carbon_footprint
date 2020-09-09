3aced5ff496812d6e276332dd5a9ae2d688c319a
Duncan Ryan <duncancryan1@gmail.com>
2020-09-09 01:33:30 +0100

    results spec all passing, can't think of any other tests it should have at the moment

--- client/tests/unit/results.spec.js
+++ client/tests/unit/results.spec.js
@@ -1,5 +1,31 @@
 import { shallowMount } from '@vue/test-utils';
 import Results from '@/components/Results.vue';
-import { questionsData } from './questions.';
+import { questionsData } from './questions';
 import { usersData } from './users';
-import { emissionsData } from './emissions'
+import { emissionsData } from './emissions';
+
+describe("Results.vue", () => {
+  let wrapper;
+  beforeEach(async () => {
+      wrapper = shallowMount(Results, {
+          propsData: {
+              selectedUser: usersData[1],
+              globalEmissions: emissionsData,
+              questions: questionsData
+          }
+      });
+      await wrapper.vm.$nextTick();
+  });
+
+  it('should have a selectedUser prop', () => {
+    expect(wrapper.vm.selectedUser.name).toMatch("Ally");
+  });
+
+  it('should have an array of globalEmissions data as a prop', () => {
+    expect(wrapper.vm.globalEmissions).toHaveLength(3);
+  });
+
+  it('should have an array of questions as a prop', () => {
+    expect(wrapper.vm.questions).toHaveLength(3);
+  });
+});
